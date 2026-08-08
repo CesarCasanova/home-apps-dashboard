c1ce48125d3d1f86b9b35e46cdf3754ff654b2c7
Cesar Casanova <cesar.casanovab92@gmail.com>
2026-08-07 19:45:11 -0500

    fix: redesign app images

--- dashboard/src/config/apps.config.ts
+++ dashboard/src/config/apps.config.ts
@@ -14,7 +14,7 @@ export const apps: AppConfig[] = [
         description: 'Manga & Comic Book Server',
         url: 'http://localhost:25600/', // Update with your Komga URL
         icon: '/komga.png',
-        color: 'from-slate-600 to-blue-700',
+        color: 'from-blue-700 to-amber-600',
     },
     {
         id: 'jellyfin',
@@ -22,7 +22,7 @@ export const apps: AppConfig[] = [
         description: 'Media Server',
         url: 'http://localhost:8899/', // Update with your Jellyfin URL
         icon: '/jellyfin.png',
-        color: 'from-blue-700 to-cyan-700',
+        color: 'from-purple-700 to-cyan-600',
     },
     {
         id: 'metube',
@@ -30,13 +30,13 @@ export const apps: AppConfig[] = [
         description: 'Video Downloader',
         url: 'http://localhost:8081', // Update with your MeTube URL
         icon: '/metube.png',
-        color: 'from-cyan-700 to-teal-700',
+        color: 'from-rose-700 to-red-600',
     },
     {
         id: 'easynvr',
         name: 'EasyNVR',
         description: 'Network Video Recorder',
-        url: 'http://localhost:10800', // Update with your EasyNVR URL
+        url: 'http://localhost:10000', // Update with your EasyNVR URL
         icon: '/easynvr.png',
         color: 'from-teal-700 to-emerald-700',
     },
